057d6d013b657cae80d08f5e1d1171fbb989fc70
DealiAxy <dealiaxy@gmail.com>
2020-11-11 10:04:46 +0800

    配置了Django管理后台显示名称
    Signed-off-by: DealiAxy <dealiaxy@gmail.com>

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+admin.site.site_header = 'DjangoStart 管理后台'
+admin.site.site_title = 'DjangoStart 管理后台'
+admin.site.index_title = 'DjangoStart 管理后台'
+
 # Register your models here.
