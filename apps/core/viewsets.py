17f81a594309b6ff1f9d012903b88ee7a1a8f395
DealiAxy <dealiaxy@gmail.com>
2020-11-11 10:53:26 +0800

    默认应用里的core添加了完整的Swagger文档说明

--- apps/core/viewsets.py
+++ apps/core/viewsets.py
@@ -1,9 +1,17 @@
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework import permissions
 from . import models
 from . import serializers
 
 
+@method_decorator(name='list', decorator=swagger_auto_schema(operation_summary='获取所有用户资料', operation_id='获取所有用户资料',operation_description='获取所有用户资料'))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(operation_summary='获取指定用户资料'))
+@method_decorator(name='create', decorator=swagger_auto_schema(operation_summary='添加用户资料'))
+@method_decorator(name='update', decorator=swagger_auto_schema(operation_summary='修改指定用户资料'))
+@method_decorator(name='partial_update', decorator=swagger_auto_schema(operation_summary='部分修改指定用户资料'))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(operation_summary='删除指定用户资料'))
 class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
