b6383ef6c4fb937fb6a4ccce21e5aa5ba8441c58
DealiAxy <dealiaxy@gmail.com>
2021-05-30 00:09:20 +0800

    更新了drf-yasg2的依赖

--- config/urls.py
+++ config/urls.py
@@ -8,8 +8,8 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework.authtoken import views as authtoken_view
 
 # drf_yasg文档工具
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
 
 from apps.core import views
 
