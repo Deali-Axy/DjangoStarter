c2574a34f1626b0e47afb7279b0f1161ada5e6b6
DealiAxy <dealiaxy@gmail.com>
2021-12-17 11:06:07 +0800

    增加了调试模式的日志配置

--- config/logging.py
+++ config/logging.py
@@ -3,8 +3,15 @@ from datetime import datetime
 from config import env_init
 
 
-def config_logging(base_dir: str):
-    # 日志配置
+def config_logging(base_dir: str) -> dict:
+    """
+    配置日志
+
+    :param base_dir: Djanago BaseDir
+    :return: logging
+    """
+
+    # 日志目录配置
     logging_dir_config = {
         'base': os.path.join(base_dir, 'log'),
         'django': 'django',
@@ -40,7 +47,8 @@ def config_logging(base_dir: str):
             'django': {
                 'level': 'DEBUG',
                 'class': 'logging.handlers.RotatingFileHandler',
-                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['django'], f'QWebFX_{datetime.now().date()}.log'),
+                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['django'],
+                                         f'QWebFX_{datetime.now().date()}.log'),
                 'maxBytes': 1024 * 1024 * 5,  # 文件大小
                 'backupCount': 1,  # 备份份数
                 'formatter': 'standard',  # 使用哪种formatters日志格式
@@ -56,7 +64,8 @@ def config_logging(base_dir: str):
             'error': {
                 'level': 'ERROR',
                 'class': 'logging.handlers.RotatingFileHandler',
-                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['error'], f'QWebFX_Error_{datetime.now().date()}.log'),
+                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['error'],
+                                         f'QWebFX_Error_{datetime.now().date()}.log'),
                 'maxBytes': 1024 * 1024 * 5,
                 'backupCount': 3,
                 'formatter': 'standard',
@@ -69,7 +78,8 @@ def config_logging(base_dir: str):
             'request_handler': {
                 'level': 'DEBUG',
                 'class': 'logging.handlers.RotatingFileHandler',
-                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['request'], f'QWebFX_Request_{datetime.now().date()}.log'),
+                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['request'],
+                                         f'QWebFX_Request_{datetime.now().date()}.log'),
                 'maxBytes': 1024 * 1024 * 5,
                 'backupCount': 3,
                 'formatter': 'standard',
@@ -77,7 +87,8 @@ def config_logging(base_dir: str):
             'scripts_handler': {
                 'level': 'DEBUG',
                 'class': 'logging.handlers.RotatingFileHandler',
-                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['script'], f'QWebFX_Script_{datetime.now().date()}.log'),
+                'filename': os.path.join(logging_dir_config['base'], logging_dir_config['script'],
+                                         f'QWebFX_Script_{datetime.now().date()}.log'),
                 'maxBytes': 1024 * 1024 * 5,
                 'backupCount': 3,
                 'formatter': 'standard',
@@ -113,3 +124,36 @@ def config_logging(base_dir: str):
     }
 
     return logging
+
+
+def config_debug_logging() -> dict:
+    """
+    调试模式的日志配置
+
+    :return: logging
+    """
+
+    console_handler = {
+        'level': 'DEBUG',
+        'class': 'logging.StreamHandler',
+        'formatter': 'standard'
+    }
+    return {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s [%(pathname)s:%(funcName)s:%(lineno)d] [%(levelname)s] %(message)s'
+            }
+        },
+        'handlers': {
+            'console': console_handler,
+        },
+        'loggers': {
+            'common': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': True
+            },
+        }
+    }
