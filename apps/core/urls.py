14602b5da9bdb448905a2360c5d87aa58e226719
DealiAxy <dealiaxy@gmail.com>
2022-04-09 22:08:02 +0800

    封装了验证码功能

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,10 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from . import viewsets
+from . import views
+
 
 router = DefaultRouter()
 router.register('user_profile', viewsets.UserProfileViewSet, basename='user_profile')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('refresh_captcha', views.refresh_captcha),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-
+from drf_yasg2.utils import swagger_auto_schema
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 from ratelimit.decorators import ratelimit
 
 
@@ -7,3 +10,16 @@ from ratelimit.decorators import ratelimit
 @ratelimit(key='ip', rate='5/m', block=True)
 def extend_admin_login(request, extra_context=None):
     return admin.site.login(request, extra_context)
+
+
+@swagger_auto_schema(method='get', operation_summary='刷新验证码')
+@permission_classes([permissions.AllowAny])
+@api_view()
+def refresh_captcha(request):
+    from contrib import captcha
+    captcha_item = captcha.refresh()
+    return Response({
+        "key": captcha_item.key,
+        "image_url": captcha_item.image_url,
+        "audio_url": captcha_item.audio_url,
+    })
