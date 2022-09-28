87a34e1aeae50364ea92f3f7a7da0f4fb34fadcb
DealiAxy <dealiaxy@gmail.com>
2022-09-28 22:51:35 +0800

    设定默认分页大小

--- django_starter/core/paginator/__init__.py
+++ django_starter/core/paginator/__init__.py
@@ -19,7 +19,7 @@ class NumberPaginator(PageNumberPagination):
 
     def __init__(
             self,
-            page_size,
+            page_size=10,
             page_size_query_param='page_size',
             page_query_param='page',
             max_page_size=None
