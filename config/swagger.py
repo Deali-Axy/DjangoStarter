95f8e44069463bbb6be1f6f65692fcfa1d950754
DealiAxy <dealiaxy@gmail.com>
2022-09-28 22:51:51 +0800

    完善swagger配置

--- config/swagger.py
+++ config/swagger.py
@@ -6,9 +6,10 @@ class CustomSwaggerAutoSchema(SwaggerAutoSchema):
     def get_tags(self, operation_keys=None):
         tags = super().get_tags(operation_keys)
 
-        if "v1" in tags and operation_keys:
+        # 处理一下swagger的文档tag，防止django-starter的接口都堆在一起
+        if "django-starter" in tags and operation_keys:
             # NOTE: `operation_keys` is a list like ['v1', 'user_log', 'create']
-            tags[0] = operation_keys[1]
+            tags[0] = f'django-starter-{operation_keys[1]}'
 
         return tags
 
@@ -24,9 +25,25 @@ class CustomOpenAPISchemaGenerator(OpenAPISchemaGenerator):
 
         swagger.tags = [
             {
-                'name': 'core',
-                'description': '核心功能'
-            }
+                'name': 'demo',
+                'description': '例子代码'
+            },
+            {
+                'name': 'django-starter-auth',
+                'description': '认证'
+            },
+            {
+                'name': 'django-starter-captcha',
+                'description': '验证码'
+            },
+            {
+                'name': 'django-starter-config',
+                'description': '本地配置中心'
+            },
+            {
+                'name': 'django-starter-oauth',
+                'description': '第三方登录'
+            },
         ]
 
         return swagger
