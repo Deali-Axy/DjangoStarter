7c4cbc25bce2353a39302faf9c40ebae8fd47ffb
DealiAxy <dealiaxy@gmail.com>
2024-07-22 21:45:56 +0800

    🍅修改默认主题

--- src/config/settings/components/simpleui.py
+++ src/config/settings/components/simpleui.py
@@ -1,7 +1,7 @@
 from config.settings.components.common import URL_PREFIX
 
 # SimpleUI 配置
-SIMPLEUI_DEFAULT_THEME = 'admin.lte.css'  # 默认主题
+SIMPLEUI_DEFAULT_THEME = 'purple.css'  # 默认主题
 # SIMPLEUI_LOGO = f'/{URL_PREFIX}static/admin/images/custom_logo.png'
 SIMPLEUI_HOME_PAGE = f'/{URL_PREFIX}django-starter/admin/extend_home/'
 SIMPLEUI_HOME_ICON = 'fa fa-home'
@@ -11,7 +11,7 @@ SIMPLEUI_HOME_ACTION = True  # 最近动作
 SIMPLEUI_ANALYSIS = False  # 关闭使用分析
 SIMPLEUI_STATIC_OFFLINE = True  # 离线模式
 SIMPLEUI_ICON = {
-    'Core': 'fa fa-cat',
+    '示例应用': 'fa fa-cat',
     '令牌': 'fa fa-lock',
     '认证令牌': 'fa fa-lock',
 }
