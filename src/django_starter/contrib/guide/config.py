68d74016ad79e923f5857662d307128628475260
DealiAxy <dealiaxy@gmail.com>
2025-03-05 15:49:06 +0800

    🍺修改了引导页的项目说明，换成了v3版本的；修改了相关项目和博客；添加了查看更多链接

--- src/django_starter/contrib/guide/config.py
+++ src/django_starter/contrib/guide/config.py
@@ -1,24 +1,27 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Uri:
     title: str
     url: str
 
+
 # 相关博客列表
 BLOGS = [
+    Uri('项目完成小结：使用DjangoStarter v3和Taro开发的微信小程序', 'https://www.cnblogs.com/deali/p/18411349'),
     Uri('项目完成小结 - Django-React-Docker-Swag部署配置', 'https://www.cnblogs.com/deali/p/16961771.html'),
-    Uri('Django更换数据库和迁移数据方案', 'https://www.cnblogs.com/deali/p/16884908.html'),
     Uri('DjangoAdmin使用合集，它的功能比你想象的强大！', 'https://www.cnblogs.com/deali/p/16678014.html'),
     Uri('轻量级消息队列 Django-Q 轻度体验', 'https://www.cnblogs.com/deali/p/16644989.html'),
-    Uri('Django-Import-Export插件控制数据导入流程', 'https://www.cnblogs.com/deali/p/16636562.html'),
     Uri('Django数据库性能优化之 - 使用Python集合操作', 'https://www.cnblogs.com/deali/p/16449011.html'),
+    Uri('如何优雅地在django项目里生成不重复的ID？', 'https://www.cnblogs.com/deali/p/18593862'),
+    Uri('新版的Django Docker部署方案，多阶段构建、自动处理前端依赖', 'https://www.cnblogs.com/deali/p/18357853'),
 ]
 
 # 相关项目列表
 PROJECTS = [
-    Uri('excel_to_model', 'https://github.com/Deali-Axy/excel_to_model'),
+    Uri('SiteDirectory', 'https://github.com/Deali-Axy/SiteDirectory'),
+    Uri('CodePackager', 'https://github.com/star-plan/code-packager'),
     Uri('django / channels', 'https://github.com/django/channels'),
-    Uri('Koed00 / django-q', 'https://github.com/Koed00/django-q'),
-    Uri('axnsan12 / drf-yasg', 'https://github.com/axnsan12/drf-yasg'),
+    Uri('excel_to_model', 'https://github.com/Deali-Axy/excel_to_model'),
 ]
