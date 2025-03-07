0b67111eff57a207ac547f5e49436a22310469dd
DealiAxy <dealiaxy@gmail.com>
2025-03-07 18:36:42 +0800

    🐎优化了一下about页面

--- src/django_starter/contrib/about/views.py
+++ src/django_starter/contrib/about/views.py
@@ -3,19 +3,23 @@ from django.contrib import messages
 from .models import About, Contact
 from .forms import ContactForm
 
+
 def index(request):
     # 获取最新的About数据
     about = About.objects.first()
-    
+
     # 如果数据库中没有数据，使用默认值
     if not about:
         about = {
             'story': '我们是一个充满激情的团队，致力于为用户提供最优质的服务。我们相信通过不断创新和改进，可以为用户带来更好的体验。',
             'mission': '通过技术创新推动行业发展，为用户创造价值，让科技更好地服务于人。',
             'values': [
-                {'title': '创新', 'description': '持续创新是我们的核心驱动力，我们始终保持开放和进取的心态。'},
-                {'title': '品质', 'description': '追求卓越品质，为客户提供最优质的产品和服务。'},
-                {'title': '协作', 'description': '团队协作是我们的基石，共同创造更大的价值。'}
+                {'title': '创新', 'icon': 'fa-brands fa-bluesky',
+                 'description': '持续创新是我们的核心驱动力，我们始终保持开放和进取的心态。'},
+                {'title': '品质', 'icon': 'fa-solid fa-shield-halved',
+                 'description': '追求卓越品质，为客户提供最优质的产品和服务。'},
+                {'title': '协作', 'icon': 'fa-solid fa-handshake-simple',
+                 'description': '团队协作是我们的基石，共同创造更大的价值。'}
             ],
             'milestones': [
                 {'year': '2020', 'title': '公司成立', 'description': '公司成立，开始专注于技术创新和产品研发'},
@@ -33,8 +37,9 @@ def index(request):
             'phone': '400-123-4567',
             'address': '中国北京市朝阳区科技园区888号'
         }
-    
+
     return render(request, 'django_starter/about/index.html', {'about': about})
+
 
 def contact(request):
     about = About.objects.first()
@@ -57,8 +62,10 @@ def contact(request):
         'about': about
     })
 
+
 def privacy_policy(request):
     return render(request, 'django_starter/about/privacy_policy.html')
+
 
 def terms_of_service(request):
     return render(request, 'django_starter/about/terms_of_service.html')
