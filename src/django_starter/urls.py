2dd482c71fbfc3b77886bfa85d3676fcc2917d24
DealiAxy <dealiaxy@gmail.com>
2025-05-14 00:26:29 +0800

    feat(监控): 添加Prometheus中间件和健康检查端点
    引入Prometheus中间件以收集HTTP请求计数和延迟指标，并添加健康检查端点用于监控系统状态。同时，新增/metrics端点以提供Prometheus监控数据。

--- src/django_starter/urls.py
+++ src/django_starter/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from django_starter.contrib.guide import views
+from django_starter.contrib.guide import views as guide_views
+from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', guide_views.index),
     path('admin/', include('django_starter.contrib.admin.urls')),
+    path('health/', views.health_check, name='health_check'),
+    path('metrics/', views.metrics, name='metrics'),
 ]
