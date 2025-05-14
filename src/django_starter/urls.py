a8ed716b97c888bde82e67c25443cacba8536497
DealiAxy <dealiaxy@gmail.com>
2025-05-14 11:05:23 +0800

    feat(monitoring): 添加监控和健康检查模块
    引入新的监控和健康检查模块，包括健康检查端点、Prometheus指标端点和相关配置。该模块用于监控系统的健康状况和性能指标，便于在容器化环境中进行健康检查和性能监控。

--- src/django_starter/urls.py
+++ src/django_starter/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
 from django_starter.contrib.guide import views as guide_views
-from . import views
 
 urlpatterns = [
     path('', guide_views.index),
     path('admin/', include('django_starter.contrib.admin.urls')),
-    path('health/', views.health_check, name='health_check'),
-    path('metrics/', views.metrics, name='metrics'),
+    path('monitoring/', include('django_starter.contrib.monitoring.urls')),
 ]
