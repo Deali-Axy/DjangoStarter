c4dfd23bbc17667b3d164f858067c79fcf253393
DealiAxy <dealiaxy@gmail.com>
2026-01-21 09:12:46 +0800

    fix(csp): 添加UNSAFE_EVAL和UNSAFE_INLINE到安全策略
    允许使用eval和内联样式以满足特定功能需求

--- src/config/settings/components/csp.py
+++ src/config/settings/components/csp.py
@@ -4,8 +4,8 @@ SECURE_CSP = {
     # To enforce a CSP policy:
     "default-src": [CSP.SELF],
     # Allow self-hosted scripts and script tags with matching `nonce` attr.
-    "script-src": [CSP.SELF, CSP.NONCE],
-    "style-src": [CSP.SELF, CSP.NONCE],
+    "script-src": [CSP.SELF, CSP.NONCE, CSP.UNSAFE_EVAL, CSP.UNSAFE_INLINE],
+    "style-src": [CSP.SELF, CSP.NONCE, CSP.UNSAFE_INLINE],
     "img-src": [CSP.SELF, "https:", "data:"],
     "font-src": [CSP.SELF, "https:", "data:"],
     "connect-src": [CSP.SELF],
