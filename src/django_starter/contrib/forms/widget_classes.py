a844418b95565516f86835317db2b6aef44b834a
DealiAxy <dealiaxy@gmail.com>
2026-01-22 13:48:33 +0800

    style(ui): 移除表单元素的input-bordered类以简化样式
    统一移除了多个表单元素中的input-bordered类，保持样式简洁一致

--- src/django_starter/contrib/forms/widget_classes.py
+++ src/django_starter/contrib/forms/widget_classes.py
@@ -1,14 +1,14 @@
 # widget_classes.py
 
-TEXT_INPUT_CLASS = 'input input-bordered w-full'
+TEXT_INPUT_CLASS = 'input w-full'
 
-TEXT_AREA_CLASS = 'textarea textarea-bordered w-full'
+TEXT_AREA_CLASS = 'textarea w-full'
 
-SELECT_CLASS = 'select select-bordered w-full'
+SELECT_CLASS = 'select w-full'
 
 # 密码输入框可以复用TextInput的样式
 PASSWORD_INPUT_CLASS = TEXT_INPUT_CLASS
 
-DATE_INPUT_CLASS = 'input input-bordered w-full'
+DATE_INPUT_CLASS = 'input w-full'
 
-NUMBER_INPUT_CLASS = 'input input-bordered w-full'
+NUMBER_INPUT_CLASS = 'input w-full'
